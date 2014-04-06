890e190415f804486d39495b819a59c82f6e0f37
LoadingPleaseWait <anonymous9304@gmail.com>
2014-04-06 15:23:18 -0500

    disable auton

--- src/com/bellaire/aerbot/listeners/AutonomousListener.java
+++ src/com/bellaire/aerbot/listeners/AutonomousListener.java
@@ -4,7 +4,7 @@ import com.bellaire.aerbot.Environment;
 
 public class AutonomousListener implements Listener {
     
-	public static double WAIT_LENGTH = 0;
+	public static final int WAIT_LENGTH = 7500;
 	
     private Environment env;
     private long time = -1;
@@ -12,7 +12,6 @@ public class AutonomousListener implements Listener {
     
     public void init(Environment env) {
         this.env = env;
-        delay = System.currentTimeMillis();
         time = -1;
     }
 
@@ -25,22 +24,25 @@ public class AutonomousListener implements Listener {
     }
 
     public void execute() {
-        long now = System.currentTimeMillis();
+        /*long now = System.currentTimeMillis();
+        
+        if(delay == 0)
+        	delay = now;
         
         if(now - delay >= WAIT_LENGTH && time == -1)
         	time = System.currentTimeMillis();
         
         if(now - time < 2500 && time != -1) {
             env.getWheelSystem().drive(0.5, 0.007);
-            env.getShooterSystem().open();
+            //env.getShooterSystem().open();
         } else if(now - time < 5500 && time != -1) {
             env.getWheelSystem().drive(0.1, 0);
             env.getIntakeSystem().setMotor(-1);
         }else {
         	env.getIntakeSystem().setMotor(0);
         	env.getWheelSystem().drive(0, 0);
-        	env.getShooterSystem().close();
-        }
+        	//env.getShooterSystem().close();
+        }*/
     }
     
 }
