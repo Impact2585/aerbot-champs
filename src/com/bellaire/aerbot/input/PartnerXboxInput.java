15c2a472f69086cbf7f3d7999a6f023612a24c80
LoadingPleaseWait <anonymous9304@gmail.com>
2014-04-07 16:02:06 -0500

    added IntakeSystem and XboxInput classes

---
+++ src/com/bellaire/aerbot/input/PartnerXboxInput.java
@@ -0,0 +1,83 @@
+package com.bellaire.aerbot.input;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+/*
+
+1: A
+2: B
+3: X
+4: Y
+5: Left Bumper
+6: Right Bumper
+7: Back
+8: Start
+9: Left Joystick
+10: Right Joystick
+
+*/
+
+public class PartnerXboxInput implements InputMethod {
+
+    private Joystick driver, shooter;
+    
+    public PartnerXboxInput() {
+        driver = new Joystick(1);
+        shooter = new Joystick(2);
+    }
+    
+    public double getLeftX() {
+        return driver.getRawAxis(1);
+    }
+
+    public double getRightX() {
+        return driver.getRawAxis(4);
+    }
+    
+    public double getLeftY() {
+        return driver.getRawAxis(2);
+    }
+
+    public double getRightY() {
+        return driver.getRawAxis(5);
+    }
+    
+    public boolean shift() {
+        return driver.getRawButton(2) || driver.getRawButton(6);
+    }
+    
+    public boolean catchBall() {
+        return driver.getRawButton(1);
+    }
+    
+    public boolean shoot() {
+        return shooter.getRawButton(5);
+    }
+    
+    public boolean intakeLift() {
+        return shooter.getRawButton(2);
+    }
+    
+    public boolean shooterLift() {
+        return shooter.getRawButton(4);
+    }
+    
+    public boolean directionToggle() {
+        return driver.getRawButton(4) || driver.getRawButton(6);
+    }
+    
+    public boolean intakeToggle() {
+        return shooter.getRawButton(6);
+    }
+    
+    public int intake() {
+        if(shooter.getRawButton(1)) { // outtake
+            return -1;
+        } else if(shooter.getRawButton(3)) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    
+}
---
+++ src/com/bellaire/aerbot/input/XboxInput.java
@@ -0,0 +1,81 @@
+package com.bellaire.aerbot.input;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+/*
+
+1: A
+2: B
+3: X
+4: Y
+5: Left Bumper
+6: Right Bumper
+7: Back
+8: Start
+9: Left Joystick
+10: Right Joystick
+
+*/
+
+public class XboxInput implements InputMethod {
+
+    private Joystick controller;
+    
+    public XboxInput() {
+        controller = new Joystick(1);
+    }
+    
+    public boolean intakeLift() {
+        return false;
+    }
+    
+    public double getLeftX() {
+        return controller.getRawAxis(1);
+    }
+
+    public double getRightX() {
+        return controller.getRawAxis(4);
+    }
+    
+    public double getLeftY() {
+        return controller.getRawAxis(2);
+    }
+
+    public double getRightY() {
+        return controller.getRawAxis(5);
+    }
+    
+    public boolean shift() {
+        return controller.getRawButton(2);
+    }
+    
+    public boolean catchBall() {
+        return false;
+    }
+    
+    public boolean shoot() {
+        return controller.getRawButton(1);
+    }
+    
+    public boolean shooterLift() {
+        return controller.getRawButton(4);
+    }
+    
+    public boolean intakeToggle() {
+        return controller.getRawButton(3);
+    }
+    
+    public boolean directionToggle() {
+        return false;
+    }
+    
+    public int intake() {
+        if(controller.getRawButton(5)) {
+            return 1;
+        } else if(controller.getRawButton(6)) {
+            return 2;
+        } else {
+            return 0;
+        }
+    }
+}
